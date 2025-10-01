3a39d521d63b7322f0790420cd534bb9095d2275
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:26:58 +0200

    working client

--- src/main/java/com/fortuna/Main.java
+++ src/main/java/com/fortuna/Main.java
@@ -1,7 +1,34 @@
 package com.fortuna;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("Hello world!");
+        System.out.println("Inserisci ip del server");
+        Scanner scan = new Scanner(System.in);
+
+        String ip = scan.nextLine();
+        System.out.println("Inserisci porta del server");
+        int port = Integer.parseInt(scan.nextLine());
+        
+        Socket mioSocket = new Socket(ip, port);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
+
+        System.out.println("Inserisci stringa: ");
+        String stringaMinuscola = scan.nextLine();
+        out.println(stringaMinuscola);
+        System.out.println(in.readLine());
+
+        mioSocket.close();
+        scan.close();
     }
 }
